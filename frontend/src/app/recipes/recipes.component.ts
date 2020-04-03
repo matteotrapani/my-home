2022f2753b29aab7c113dd988fdaf6204024329a
Matteo Trapani <matteo.trapani92@gmail.com>
2020-04-03 18:54:47 +0200

    Fixed cards style

--- frontend/src/app/recipes/recipes.component.ts
+++ frontend/src/app/recipes/recipes.component.ts
@@ -17,7 +17,7 @@ export class RecipesComponent implements OnInit {
 
   loadRecipes(): Array<IRecipe> {
     const result: Array<IRecipe> = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 10; i++) {
       const recipe: Recipe = new Recipe();
       recipe.id = i.toString();
       recipe.name = `Torta di mele ${i}`;
