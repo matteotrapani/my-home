b335a873eae8aba9ea2a9a76187a1058ca855143
Matteo Trapani <matteo.trapani92@gmail.com>
2020-04-21 12:44:53 +0200

    Implemented auto reload forrecipes

--- frontend/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,18 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IRecipe} from '../../models/recipe.model';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import {map, shareReplay} from 'rxjs/operators';
+import {filter, map, pairwise, shareReplay, startWith, takeUntil} from 'rxjs/operators';
 import {RecipesService} from '../../services/recipes.service';
+import {NavigationEnd, Router, RouterEvent} from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-  constructor(private recipesService: RecipesService, private breakpointObserver: BreakpointObserver) {
+export class RecipeListComponent implements OnInit, OnDestroy {
+  constructor(
+    private recipesService: RecipesService,
+    private breakpointObserver: BreakpointObserver,
+    private router: Router) {
   }
+  public destroyed = new Subject<any>();
   recipes: Array<IRecipe> = [];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -21,7 +26,24 @@ export class RecipeListComponent implements OnInit {
     );
 
   ngOnInit(): void {
-    this.fetchRecipes();
+    this.setAutoReloadOnFirstLoadAndRouteChange();
+  }
+
+  private setAutoReloadOnFirstLoadAndRouteChange() {
+    this.router.events.pipe(
+      filter((event: RouterEvent) => event instanceof NavigationEnd),
+      pairwise(),
+      filter((events: RouterEvent[]) => events[0].url.startsWith(events[1].url)),
+      startWith('Initial call'),
+      takeUntil(this.destroyed)
+    ).subscribe(() => {
+      this.fetchRecipes();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
   fetchRecipes(): void {
