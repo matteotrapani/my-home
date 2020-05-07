ce6964afeea3a8f924dfe308787412c97bde6e79
Matteo Trapani <matteo.trapani92@gmail.com>
2020-05-07 15:07:03 +0200

    PWA settings code review

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,33 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {Platform} from '@angular/cdk/platform';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PwaService} from '../services/pwa.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  private promptEvent: any;
-
+export class HomeComponent implements OnInit, OnDestroy {
+  public showButton: boolean;
+  private showButtonSubscription: Subscription;
   constructor(private pwaService: PwaService) {
-    alert('home');
   }
 
   ngOnInit(): void {
+    this.showButtonSubscription = this.pwaService.showInstallButton.subscribe(canShow => this.showButton = canShow);
+  }
+  ngOnDestroy(): void {
+    this.showButtonSubscription.unsubscribe();
   }
 
   installPwa() {
     this.pwaService.promptInstallation();
-    // try {
-    //   alert('install PWA');
-    //   alert(this.promptEvent);
-    //   this.promptEvent.prompt();
-    //
-    //   alert('installed PWA');
-    // } catch (e) {
-    //   alert(e);
-    // }
   }
 
 }
--- frontend/src/app/services/pwa.service.ts
+++ frontend/src/app/services/pwa.service.ts
@@ -1,47 +1,38 @@
 import {HostListener, Injectable} from '@angular/core';
 import {Platform} from '@angular/cdk/platform';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PwaService {
   private promptEvent: any;
+  public showInstallButton = new BehaviorSubject(false);
 
   constructor(private platform: Platform) {
-    alert('service');
   }
 
 
   @HostListener('window:beforeinstallprompt', ['$event'])
   onbeforeinstallprompt(e) {
-    alert('beforeinstallprompt');
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     this.promptEvent = e;
-    // this.showButton = true;
+    this.showInstallButton.next(false);
   }
 
-  initPwa() {
-    // if (this.platform.ANDROID) {
-    //   window.addEventListener('beforeinstallprompt', (event: any) => {
-    //     alert('beforeinstallprompt');
-    //     event.preventDefault();
-    //     this.promptEvent = event;
-    //   });
-    // }
-  }
+  initPwa() {}
 
   promptInstallation() {
     try {
-      alert('install PWA');
-      alert(this.promptEvent);
       this.promptEvent.prompt();
-
-      alert('installed PWA');
     } catch (e) {
-      alert(e);
+      alert('An error occurred during installation');
     }
-    // this.promptEvent.prompt();
+  }
+
+  canShowButton() {
+    return this.showInstallButton;
   }
 }
